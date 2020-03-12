27aaab65d9317862d18d70546b27ea453ced9024
Student_3 <student_3@PS-DAL-M3.local>
2020-03-12 11:31:08 -0500

    adding obj(constr)

--- pseudocode/script.js
+++ pseudocode/script.js
@@ -1,2 +1,31 @@
 // pseudo code 
 
+//object constructor for characters. 
+
+// Initialize constructor for characters 
+
+class characters {
+    constructor(health,attack,defense,special) {
+        this.health = health;
+        this.attack = attack;
+        this.defense = defense;
+        this.special = special;
+    }
+
+    get getHealth() {
+        return this.health;
+    }
+
+    
+}
+
+  
+  // Initialize individual character instances
+ 
+
+
+//character selection 2 players
+
+// functionality needed for user selection: 
+// >make images draggable to characterChoice Box. 
+
