2123ea9fe4fd125e983d210ee8724edb8787bee2
Student_3 <student_3@PS-DAL-M3.local>
2020-03-12 12:37:46 -0500

    update js

--- pseudocode/manageGame.js
+++ pseudocode/manageGame.js
@@ -1,9 +1,11 @@
 //game start function/ need methods to create player in game and game play
+//we can only call one function w/in onclick event. so must place other methods in game start method
 
 let manageGame = {
 
     gameStart: function() {
-
+        this.resetPlayer();
+        this.setBattle();
     },
 
     resetPlayer: function() {
