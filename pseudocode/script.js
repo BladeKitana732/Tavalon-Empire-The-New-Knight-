c9fcd042a0647fdb7b5fc8ef480aefd25aa0f5e7
Student_3 <student_3@PS-DAL-M3.attlocal.net>
2020-03-14 02:00:24 -0500

    updated function for displaying ONLY choices needed to continue on journey

--- pseudocode/script.js
+++ pseudocode/script.js
@@ -68,14 +68,35 @@ function startJourney () {
 //using method of .text on const textNode to pull journey state/path player is currently in the story game  
 function textNode (textNodeIndex) {
     const textNode = textNodes.find(textNode => textNode.path === textNodeIndex)
-    textElement.innerText = textNode.text
+    textElement.innerText = textNode.text;
+    //while loop to show choices that affect THAT PATH ONLY (NOT EVERY PATH WILL HAVE 4 CHOICES) using child method b/c i made a choice button element (const buttonsElement) with a div and the buttons (child divs) is controlling node/story
+
+    while(buttonsElement.firstChild) {
+        buttonsElement.removeChild(buttonsElement.firstChild)
+
+    }
+    //this is going to pull choices needed to be seen only based on state/journey path with if statement & eventlistner
+    textNode.choices.forEach(choice => {
+        if(choiceSelection(choice)) {
+            const button = document.createElement('button')
+            button.innerText = choice.text
+            button.classList.add('btn')
+            button.addEventListener('click', () => choiceSelection(choice))
+            buttonsElement.appendChild(button)
+        }
+
+    })
+
+
+
+
 }
 
 function choiceSelection (choice) {
-
+    return true
 }
 
-
+//variable of paths and choices(it is an array with nested objects to go iterate through the paths with properties of path and text to show current stage in story mode and objects w/in these objects to iterate through the choices and next action)
 const textNodes = [
     {
         path: 1,
@@ -91,27 +112,12 @@ const textNodes = [
             text: 'choice 2',
             nextNode: 2,
 
-         },
-    
-         {
-             id: 2
          }
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
         ]
 
-
     }
-
 
 ]
 
-startJourney()
+startJourney();
