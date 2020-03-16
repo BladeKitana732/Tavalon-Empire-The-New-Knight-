9798daef186f5e114ecd211b413131d79a44d5ce
Student_3 <student_3@PS-DAL-M3.attlocal.net>
2020-03-16 15:44:04 -0500

    cleaning out player.js file

--- pseudocode/manageGame.js
+++ pseudocode/manageGame.js
@@ -1,3 +1,16 @@
+//declare global player variable to be ran to choose different character options
+let player;
+
+//function/method object? to be used in selecting player function/method object 
+    function Character(characterType, health, attack, defense) {
+          this.characterType = characterType;
+          this.health = health;
+          this.attack = attack;
+          this.defense = defense;
+          // this.special = special; 
+    }
+  
+
 //game start function/ need methods to create player in game and game play
 //we can only call one function w/in onclick event. so must place other methods in game start method
 
@@ -5,7 +18,7 @@ let Game = {
 
     gameStart: function(characterType) {
         this.resetPlayer(characterType);
-        this.setBattle();
+        
     },
 
     //using switch statement to run through character options and to easily break when selection has been made
@@ -24,7 +37,7 @@ let Game = {
                 player = new Character(characterType,100,30,70);
                 break;
         }
-        //variable to link with html div id="character boxes"
+  //variable to link with html div id="character boxes"
 
         let getCharacter = document.querySelector(".wrapper");
         getCharacter.innerHTML = '<img src="wireframes/characters/' + characterType.toLowerCase() + '.jpg" class="wireframes/characters"><div><h3>' + characterType + '</h3><p>Health: ' + player.health + '</p><p>Attack: ' + player.attack + '</p><p>Defense: ' + player.defense + '</p></div>';
--- pseudocode/player.js
+++ pseudocode/player.js
@@ -1,22 +1,22 @@
-//declare global player variable to be ran to choose different character options
-let player;
+// //declare global player variable to be ran to choose different character options
+// let player;
 
-//function/method object? to be used in selecting player function/method object 
-function Character(characterType, health, attack, defense) {
-    this.characterType = characterType;
-    this.health = health;
-    this.attack = attack;
-    this.defense = defense;
-    // this.special = special; 
-}
+// //function/method object? to be used in selecting player function/method object 
+// function Character(characterType, health, attack, defense) {
+//     this.characterType = characterType;
+//     this.health = health;
+//     this.attack = attack;
+//     this.defense = defense;
+//     // this.special = special; 
+// }
 
-//global enemy variable 
-let enemy;
+// //global enemy variable 
+// let enemy;
 
-function Opponent(opponentType, health, attack, defense) {
-    this.opponentType = opponentType;
-    this.health = health;
-    this.attack = attack;
-    this.defense = defense;
-    // this.special = special; 
-}
+// function Opponent(opponentType, health, attack, defense) {
+//     this.opponentType = opponentType;
+//     this.health = health;
+//     this.attack = attack;
+//     this.defense = defense;
+//     // this.special = special; 
+// }
