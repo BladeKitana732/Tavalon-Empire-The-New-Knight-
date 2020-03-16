d837b2fccb6844312e8835d93c56ce80437a778f
Student_3 <student_3@PS-DAL-M3.attlocal.net>
2020-03-16 16:36:49 -0500

    got attack button linked to dom to show health decrease after clicked!

---
+++ pseudocode/battle.js
@@ -0,0 +1,38 @@
+//battle functioning testing
+
+let user = {
+    health: 100,
+    defense: 100,
+    power: 20
+}
+
+let enemy = {
+    health: 100,
+    defense: 100,
+    power: 15
+}
+
+//attack function to call in button when clicked in HTML 
+const attack = () => {
+    let userAttack = Math.floor(Math.random() * user.power);
+    enemy.health -= userAttack;
+    showInDom()
+}
+
+
+
+
+
+
+
+
+//empty function to call and show health in DOM
+const showInDom = () => {
+    document.getElementById('enemy-health').innerText =
+    enemy.health;
+
+    document.getElementById('user-health').innerText =
+    user.health;
+}
+
+showInDom();
--- pseudocode/script.js
+++ pseudocode/script.js
@@ -5,25 +5,25 @@
 
 // code 1 test: Initialize class constructor for characters 
 
-class character {
-    constructor(health,attack,defense,) {
-        this.health = health;
-        this.attack = attack;
-        this.defense = defense;
+// class character {
+//     constructor(health,attack,defense,) {
+//         this.health = health;
+//         this.attack = attack;
+//         this.defense = defense;
 
-    }
+//     }
 
 
-}
+// }
 
 
 //code 2 test: object function (blueprint to run new variables through)
-function Characters(name,health, attack, defense) {
-    this.name = name;
-    this.health = health;
-    this.attack = attack;
-    this.defense = defense;
-}
+// function Characters(name,health, attack, defense) {
+//     this.name = name;
+//     this.health = health;
+//     this.attack = attack;
+//     this.defense = defense;
+// }
 
 //instance for code 2 test
 
@@ -47,105 +47,105 @@ console.log(Necromancer)
 
 //test 1 
 
-const textElement = document.getElementById('textElement');
-const choiceButtonsElement = document.getElementById('choice-buttons');
+// const textElement = document.getElementById('textElement');
+// const choiceButtonsElement = document.getElementById('choice-buttons');
 
-//keeps track of choices made
-let state = {};
+// //keeps track of choices made
+// let state = {};
 
 
-//starts game
-function startJourney() {
-    state = {}
-    pathArrayDisplay(1)
+// //starts game
+// function startJourney() {
+//     state = {}
+//     pathArrayDisplay(1)
     
-  }
+//   }
 
-  //this function is to ensure i am only displaying the choices that are needed choose from 
-  function showSelection(choice) {
-    return choice.requiredState == null || choice.requiredState(state)
-  }
+//   //this function is to ensure i am only displaying the choices that are needed choose from 
+//   function showSelection(choice) {
+//     return choice.requiredState == null || choice.requiredState(state)
+//   }
   
-  //this function is to ensure that AFTER player makes selection, it will continue on with journey with current choices based on current path number it is on. 
-  function choiceSelection(choice) {  
-    const nextPathNum = choice.nextPath
-    state = Object.assign(state, choice.setState)
-    pathArrayDisplay(nextPathNum)
-  }
+//   //this function is to ensure that AFTER player makes selection, it will continue on with journey with current choices based on current path number it is on. 
+//   function choiceSelection(choice) {  
+//     const nextPathNum = choice.nextPath
+//     state = Object.assign(state, choice.setState)
+//     pathArrayDisplay(nextPathNum)
+//   }
   
-  //this function is to pull the information from the array to be able to display selections/choices based on current path in DOM
-  function pathArrayDisplay(arrayIndex) {
-    const selection = pathArray.find(selection => selection.path === arrayIndex)
-    textElement.innerText = selection.text
+//   //this function is to pull the information from the array to be able to display selections/choices based on current path in DOM
+//   function pathArrayDisplay(arrayIndex) {
+//     const selection = pathArray.find(selection => selection.path === arrayIndex)
+//     textElement.innerText = selection.text
     
-//while loop to remove all options to add options that are needed only
-    while(choiceButtonsElement.firstChild) {
-      choiceButtonsElement.removeChild(choiceButtonsElement.firstChild)
-    }
+// //while loop to remove all options to add options that are needed only
+//     while(choiceButtonsElement.firstChild) {
+//       choiceButtonsElement.removeChild(choiceButtonsElement.firstChild)
+//     }
   
-//pulling selection variable which holds the data of text needed to be displayed for choice selections with event listener to newly created button element to call choices properly through each selection 
-    selection.choices.forEach(choice => {
-      if(showSelection(choice)){
-        const button = document.createElement('button')
-        button.innerText = choice.text
-        button.classList.add('btn')
-        button.addEventListener('click', () => choiceSelection(choice))
-        choiceButtonsElement.appendChild(button)
-      }
-    })
-  }
+// //pulling selection variable which holds the data of text needed to be displayed for choice selections with event listener to newly created button element to call choices properly through each selection 
+//     selection.choices.forEach(choice => {
+//       if(showSelection(choice)){
+//         const button = document.createElement('button')
+//         button.innerText = choice.text
+//         button.classList.add('btn')
+//         button.addEventListener('click', () => choiceSelection(choice))
+//         choiceButtonsElement.appendChild(button)
+//       }
+//     })
+//   }
   
-  //array of path objects with nested objects for buttons/selections needed to be made for user to continue journey
-  const pathArray = [
-    {
-      path: 1,
-      text: 'path1',
-      choices: [
-        {
-          text: 'button1',
-          setState: { choice1: true},
-          nextPath: 2,
-        },
+//   //array of path objects with nested objects for buttons/selections needed to be made for user to continue journey
+//   const pathArray = [
+//     {
+//       path: 1,
+//       text: 'path1',
+//       choices: [
+//         {
+//           text: 'button1',
+//           setState: { choice1: true},
+//           nextPath: 2,
+//         },
   
-        {
-          text: 'button2',
-          nextPath: 2
-        },
-      ]
-    },
+//         {
+//           text: 'button2',
+//           nextPath: 2
+//         },
+//       ]
+//     },
   
-    {
-      path: 2,
-      text: 'path2',
-      choices: [
-        {
-          text: 'path2button1',
-          requiredState: (currentState) => currentState.choice1,
-          setState: {path2text: true, choice1: false},
-          nextPath: 3
+//     {
+//       path: 2,
+//       text: 'path2',
+//       choices: [
+//         {
+//           text: 'path2button1',
+//           requiredState: (currentState) => currentState.choice1,
+//           setState: {path2text: true, choice1: false},
+//           nextPath: 3
   
-        },
+//         },
   
-        {
-          text: 'path2button2',
-          requiredState: (currentState) => currentState.choice1,
-          setState: {path2text: true, choice1: false},
-          nextPath: 3
-        },
+//         {
+//           text: 'path2button2',
+//           requiredState: (currentState) => currentState.choice1,
+//           setState: {path2text: true, choice1: false},
+//           nextPath: 3
+//         },
   
-        {
-          text: 'path2button3',
-          nextPath: 3
-        }
-      ]
+//         {
+//           text: 'path2button3',
+//           nextPath: 3
+//         }
+//       ]
   
-    }
-  ]
+//     }
+//   ]
   
-  startJourney()
+//   startJourney()
 
-  //battle
+//   //battle
 
-  setBattle: function () {
+//   setBattle: function () {
 
-  },
+//   },
