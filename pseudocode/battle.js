9ec2dbb6a872bdf6e7c4c32b3c9126063779a4c3
Student_3 <student_3@PS-DAL-M3.attlocal.net>
2020-03-16 17:00:24 -0500

    updated button click to disable after attack has been made/so user cannot keep clicking w/o enemy attacking back

--- pseudocode/battle.js
+++ pseudocode/battle.js
@@ -14,25 +14,29 @@ let enemy = {
 
 //attack function to call in button when clicked in HTML 
 const attack = () => {
+    let attackButton = document.getElementById('attack-button');
+    
+    attackButton.disabled = true;
+
+
     let userAttack = Math.floor(Math.random() * user.power);
     enemy.health -= userAttack;
-    showInDom()
+
+    showInDom();
 }
-
-
-
-
 
 
 
 
 //empty function to call and show health in DOM
 const showInDom = () => {
+
     document.getElementById('enemy-health').innerText =
     enemy.health;
 
     document.getElementById('user-health').innerText =
     user.health;
+
 }
 
 showInDom();
